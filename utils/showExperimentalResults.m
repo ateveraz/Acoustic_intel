9051a1dc8df20d26091af11826069e87ec838c94
ateverz <alejandro.tevera@cinvestav.mx>
2025-02-21 14:58:38 +0100

    Scripts for plotting experiments

--- utils/showExperimentalResults.m
+++ utils/showExperimentalResults.m
@@ -13,31 +13,71 @@ FlAir_data = GetLoggedData(settings_json);
 dataLog = FlAir_data.loadBatch;
 
 %% Plot the data
-close all;
+close all; clc;
 
 % Plot the position of the drone
 figure;
 subplot(2,1,1);
-plot(dataLog.vrpn.t, dataLog.vrpn.x(:,1), 'b:', 'LineWidth', 2);
+plot(dataLog.socket05.t, dataLog.socket05.x(:,1), 'b:', 'LineWidth', 2);
 hold on;
-plot(dataLog.vrpn.t, dataLog.vrpn.xd(:,1), 'k', 'LineWidth', 2);
+plot(dataLog.socket05.t, dataLog.socket05.xd(:,1), 'k', 'LineWidth', 2);
+%plot(dataLog.vrpn1.t, dataLog.vrpn1.x(:,1), 'r--', 'LineWidth', 2);
 xlabel('Time [s]');
 ylabel('X [m]');
 legend({'Measured', 'Desired'}, 'Location', 'northoutside', 'Orientation', 'horizontal', 'Interpreter', 'latex');
 
 subplot(2,1,2);
-plot(dataLog.vrpn.t, dataLog.vrpn.x(:,2), 'b:', 'LineWidth', 2);
+plot(dataLog.socket05.t, dataLog.socket05.x(:,2), 'b:', 'LineWidth', 2);
 hold on;
-plot(dataLog.vrpn.t, dataLog.vrpn.xd(:,2), 'k', 'LineWidth', 2);
+plot(dataLog.socket05.t, dataLog.socket05.xd(:,2), 'k', 'LineWidth', 2);
 xlabel('Time [s]');
 ylabel('Y [m]');
 
 % Plot the XY plane trajectory
 figure;
-plot(dataLog.vrpn.x(:,1), dataLog.vrpn.x(:,2), 'b:', 'LineWidth', 2);
+plot(dataLog.socket05.x(:,1), dataLog.socket05.x(:,2), 'b:', 'LineWidth', 2);
 hold on;
-plot(dataLog.vrpn.xd(:,1), dataLog.vrpn.xd(:,2), 'k', 'LineWidth', 2);
+plot(dataLog.socket05.xd(:,1), dataLog.socket05.xd(:,2), 'k', 'LineWidth', 2);
 xlabel('X [m]');
 ylabel('Y [m]');
 legend({'Measured', 'Desired'}, 'Location', 'northoutside', 'Orientation', 'horizontal', 'Interpreter', 'latex');
 
+% Show the desired path and real trajectory using colormaps.
+step = 100;
+dataLog.socket05.t_lf = reduceSampleRate(dataLog.socket05.t, step, true);
+dataLog.socket05.x_lf = reduceSampleRate(dataLog.socket05.x, step, true);
+dataLog.socket05.xd_lf = reduceSampleRate(dataLog.socket05.xd, step, true);
+
+figure;
+patch(dataLog.socket05.x_lf(:,1), dataLog.socket05.x_lf(:,2), dataLog.socket05.t_lf, 'EdgeColor', 'interp', 'FaceColor', 'none');
+hold on
+patch(dataLog.socket05.xd_lf(:,1), dataLog.socket05.xd_lf(:,2), dataLog.socket05.t_lf, 'EdgeColor', 'interp', 'FaceColor', 'none', 'LineStyle', ':');
+colorbar;
+xlabel('X [m]');
+ylabel('Y [m]');
+legend({'Measured', 'Desired'}, 'Location', 'northoutside', 'Orientation', 'horizontal', 'Interpreter', 'latex');
+
+% Plot the tracking error
+figure;
+errorL2 = computeL2Error(dataLog);
+plot(dataLog.socket05.t, errorL2, 'b:', 'LineWidth', 2);
+hold on;
+yline(mean(errorL2), 'k', 'LineWidth', 2);
+legend({'Error', 'Mean error'}, 'Location', 'northoutside', 'Orientation', 'horizontal', 'Interpreter', 'latex');
+xlabel('Time [s]');
+ylabel('Error [m]');
+
+function new_vector = reduceSampleRate(vector, step, addNaN)
+    % Take every step-th element of the vector
+    [r,~] = size(vector);
+    i = 1:step:r;
+    new_vector = vector(i,:); 
+    if addNaN
+        new_vector(end+1,:) = NaN;
+    end
+end
+
+function errorL2 = computeL2Error(dataLog)
+    % Compute the L2 error
+    errorL2 = ((dataLog.socket05.xd(:,1) - dataLog.socket05.x(:,1)).^2 + (dataLog.socket05.xd(:,2) - dataLog.socket05.x(:,2)).^2).^0.5;
+end
