ae2cb82fad4e15999d4d4b6b5cdb79f784786bc7
ateverz <alejandro.tevera@cinvestav.mx>
2025-02-26 17:48:35 +0100

    Plotting one experiment

---
+++ utils/showDataLogged.m
@@ -0,0 +1,39 @@
+%% Script to show the experimental results
+% This script is used to show the experimental results of the FLAir
+% experiments. The data is loaded from the log files and plotted.
+%
+% Workspace cleaning. 
+clearvars; close all; clc;
+% Add path to the FLAir postprocessing functions
+addpath('FLAir_postprocessing/src/')
+settings_json = 'config/settings_showDataLogged.json';
+
+% Load the data
+FlAir_data = GetLoggedData(settings_json);
+dataLog = FlAir_data.loadBatch;
+
+%% Plot the data
+close all; clc;
+
+% Plot the position of the drone
+figure;
+plot(dataLog.problem.t, dataLog.problem.x(:,1), 'r', 'LineWidth', 2);
+
+
+figure;
+plot(dataLog.problem.t, dataLog.problem.q)
+
+function new_vector = reduceSampleRate(vector, step, addNaN)
+    % Take every step-th element of the vector
+    [r,~] = size(vector);
+    i = 1:step:r;
+    new_vector = vector(i,:); 
+    if addNaN
+        new_vector(end+1,:) = NaN;
+    end
+end
+
+function dataLog_exp = computeL2ErrorXY(dataLog_exp)
+    % Compute the L2 error
+    dataLog_exp.errorL2 = ((dataLog_exp.xd(:,1) - dataLog_exp.x(:,1)).^2 + (dataLog_exp.xd(:,2) - dataLog_exp.x(:,2)).^2).^0.5;
+end
